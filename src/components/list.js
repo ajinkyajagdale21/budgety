15a9bd0cfe1769947a2a54222e951306b1147214
ajinkyajagdale21 <68507834+ajinkyajagdale21@users.noreply.github.com>
2021-01-19 00:24:57 +0530

    update: functionality done

--- src/components/list.js
+++ src/components/list.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-function List({inc,setInc,exp,setExp}){
+function List({inc,setInc,exp,setExp})
+{
     
     const IncdeleteHandler = (income) => {
         setInc(inc.filter(el => el.id !== income.id))
@@ -11,31 +12,48 @@ function List({inc,setInc,exp,setExp}){
     
     return(
         <div>
+            {
+                 inc.map( income => (
+                    
+                    <div>
+           
+                        <div >
+                        {income.description}
+                        </div>
+           
+                        <div >
+                        {income.amt}
+                        </div>
+           
+                        <div >
+                        <button onClick = {IncdeleteHandler}>delete</button>
+                        </div>           
+           
+                    </div>
+          ))}
+            
+        
+            { 
+                exp.map( expense => (
             <div>
-              {
-                  inc.map(income=>(
-                 <div>
-                    <div>{income.description} </div>
-                    <div>{income.amt}</div>
-                    <button onClick={IncdeleteHandler(income)}>delete</button>
-                 </div>
-              ))}
+            
+                <div >
+                    {expense.description}
+                </div>
+            
+                <div >
+                    {expense.amt}
+                </div>
+            
+                <div >
+                    <button onClick = {ExpdeleteHandler} >delete</button> 
+                </div>
             </div>
-             
-             <div>
-             {
-                 exp.map(expense=>(
-                 <div>
-                    <div>{expense.description} </div>
-                    <div>{expense.amt}</div>
-                    <button onClick={ExpdeleteHandler(expense)}>delete</button>
-                 </div>
-              ))}
-            </div>
-    
+            
+            ))}    
         </div>
     )
-
 }
 
 export default List;
+
